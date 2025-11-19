88e2f581912a7ff78b97837fd6ca46ce008bd873
Gabriel <131915937+BejanGabriel@users.noreply.github.com>
2025-11-19 14:23:35 +0100

    update

---
+++ FrontEnd/javascript/03-funzioni-metodi/js/02-argomenti-parametri.js
@@ -0,0 +1,10 @@
+/*
+    I parametri di una funzione sono dei segnaposto che verranno poi valorizzati quando la funzione verrà chiamata.
+*/
+const saluta = function(name){ // variabile locale che possiamo usare nel blocco della funzione
+    console.log(`Buongiorno ${name}`);
+}
+
+saluta("Sandro"); // Passo il parametro di name quando richiamo la funzione ---> argomento.
+
+
